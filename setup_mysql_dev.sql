d6de1c1d98597cb0d3df40e8d7fd06492e14c4e1
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-19 14:01:36 +0200

    [Update] - MySQL script to create a user and database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- Creates/Setup hbnb_dev_db and user hbnb_dev
 CREATE DATABASE IF NOT EXISTS hbhb_dev_db;
 CREATE USER IF NOT EXISTS 'hbhb_dev'@'localhost' IDENTIFIED BY 'hbhb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbhb_dev_db.* TO 'hbhb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbhb_dev_db.* TO 'hbhb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
