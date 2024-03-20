962c95916c798b9369b26cbc55c88c35a75d6490
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-20 15:00:31 -0700

    Updating Task 7

--- models/place.py
+++ models/place.py
@@ -7,7 +7,9 @@ from os import getenv
 import models
 
 
-place_amenity = Table("place_amenity", metadata=Base.metadata,
+place_amenity = Table(
+        "place_amenity", 
+        Base.metadata,
         Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
         Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
 )
@@ -17,12 +19,12 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), nullable=False, ForeignKey=(cities.id))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, defeult=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
--- models/user.py
+++ models/user.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import relationship, backref
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-     __tablename__ = 'users'
+    __tablename__ = 'users'
 
-    email = Column(String(128), nullabl=False)
+    email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
