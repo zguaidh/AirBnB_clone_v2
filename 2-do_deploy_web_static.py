b4da9dc6e50aad05d14dd5962321c86f544f9d19
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-04-04 14:37:11 -0700

    [Update] - Fixed the fabfile

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,26 +13,28 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    basename = os.path.basename(archive_path)
-    tmp_archive_path = f"/tmp/{basename}"
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        put(archive_path, tmp_archive_path)
-        x_archive = "/data/web_static/releases/{}".format(
-                os.path.splitext(basename)[0]
-        )
-        run(f"mkdir -p {x_archive}")
-        run("tar -xzf {} -C {}".format(
-            tmp_archive_path, x_archive
-        ))
-        run("cp -r {} {}".format(
-            x_archive + "/web_static/*", x_archive))
-        run(f"rm -rf {x_archive + '/web_static'}")
-        run("rm -f {}".format(tmp_archive_path))
-        symlink = "/data/web_static/current"
-        run(f"rm -rf {symlink}")
-        run(f"ln -s {x_archive}/ {symlink}")
-        print("New version deployed!")
-    except Exception:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+            format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+            format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(file, name)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+            format(name)).failed is True:
+        return False
+    symlink = "/data/web_static/current"
+    run(f"rm -rf {symlink}")
+    if run(f"ln -s /data/web_static/{name}/ {symlink}").failed is True:
         return False
     return True
