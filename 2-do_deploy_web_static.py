b3b5f947b693e65bdf4a1f3760bbce7944bdeaf9
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-04-04 09:18:55 -0700

    [Update] - Deployed web static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module that archives files
+"""
+import os
+from fabric.api import *
+from datetime import datetime
+env.hosts = ["34.239.250.176", "52.201.211.251"]
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """Deploys the web static"""
+    if not os.path.exists(archive_path):
+        return False
+
+    basename = os.path.basename(archive_path)
+    tmp_archive_path = f"/tmp/{basename}"
+
+    try:
+        put(archive_path, tmp_archive_path)
+        x_archive = "/data/web_static/releases/{}".format(
+                os.path.splitext(basename)[0]
+        )
+        run(f"mkdir -p {x_archive}")
+        run("tar -xzf {} -C {} --strip-components=1".format(
+            tmp_archive_path, x_archive
+        ))
+        run("rm -rf {}".format(tmp_archive_path))
+        run("rm -rf /data/web_static/current")
+        run(f"ln -sf {x_archive}/ /data/web_static/current")
+    except Exception:
+        return False
+    return True
