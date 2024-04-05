62906b67dff9d963a079d8167ca3ca26cce47659
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-04-04 22:16:38 -0700

    [Update] - All tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-Module that archives files
-"""
-import os
-from fabric.api import *
-from datetime import datetime
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+
 env.hosts = ["34.239.250.176", "52.201.211.251"]
 
 
-def do_pack():
-    """Packs the static files"""
-    env.hosts = "localhost"
-    now = datetime.now()
-    f_now = now.strftime("%Y%m%d%H%M%S")
-    path = f"versions/web_static_{f_now}.tgz"
-    local("mkdir -p versions")
-    result = local(f"tar -cvzf {path} web_static", capture=True)
-    if not result.return_code:
-        return f"versions/{path}"
-    return None
-
-
 def do_deploy(archive_path):
-    """Deploys the web static"""
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
@@ -31,26 +25,25 @@ def do_deploy(archive_path):
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-            format(name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-            format(name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-            format(file, name)).failed is True:
+           format(file, name)).failed is True:
         return False
-    if run("rm /tmp/{}".
-            format(file)).failed is True:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}".format(name, name)).failed is True:
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-            format(name)).failed is True:
+           format(name)).failed is True:
         return False
-    symlink = "/data/web_static/current"
-    run(f"rm -rf {symlink}")
-    if run("ln -s /data/web_static/releases/{} {}".
-            format(name, symlink)).failed is True:
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
     return True
