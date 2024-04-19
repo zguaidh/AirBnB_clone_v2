ce7a05c85a87cb8c732c902c3ff5f250e06b6473
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-04-19 22:17:31 +0200

    [Update] - First routing

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that displays 'Hello HBNB!' when requesting the web
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Responds with 'Hello HBNB!' when requested
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
