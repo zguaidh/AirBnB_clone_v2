b310ad94e788b99dd564c91619a3baeb4d490ec7
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-04-21 12:47:30 +0200

    [Update] - Fixed BBaseModlel

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,6 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            print("Initializing")
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
