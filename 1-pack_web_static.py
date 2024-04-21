df7b8f14bc10d85e4d15b745b701caa5b63402b3
zguaidh <zguaidhabiba@gmail.com>
2024-04-21 09:03:56 -0700

    tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack.
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack.
 execute: fab -f 1-pack_web_static.py do_pack
 """
 
