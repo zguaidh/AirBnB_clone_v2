bebe755aa5399b8b7404a79de31d393d1e99b8e3
habiba zguaid <zguaidhabiba@gmail.com>
2024-04-21 06:16:05 -0700

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,5 +23,5 @@ sudo echo "<html>
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '/listen 80/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
