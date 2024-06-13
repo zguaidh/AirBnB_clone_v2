64d53b12cd24a88ac804bc38a2e25d097f32d987
zguaidh <zguaidhabiba@gmail.com>
2024-06-13 14:01:01 -0700

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that starts a Flask web app
+"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    all_states = storage.all(State)
+    return render_template('7-states_list.html', states=all_states)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
